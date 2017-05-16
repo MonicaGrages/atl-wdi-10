e17f136eadb490cb87fc02c7aecd3cb9e73cec73
MonicaGrages <mgrages1@gmail.com>
2017-05-16 14:02:32 -0400

    worked on setting up hbs files for pizza express

--- homework/unit_02/express/pizza-express/app.js
+++ homework/unit_02/express/pizza-express/app.js
@@ -2,6 +2,8 @@
 
 //require express package
 var express = require('express');
+//require hbs
+var hbs = require('hbs');
 //save an express module as 'app'
 var app     = express();
 // assigning 3000 as our port
@@ -9,18 +11,25 @@ var port    = 3000;
 
 // tells the server to listen for requests on port 3000
 
+app.set('view engine', 'hbs'); //tells Express what to use for rendering templates
+app.set('views', './views');
+
 app.get('/', function(req, res) {
   // when a request comes in at localhost:3000, it will respond
   res.send('Welcome to Pizza Express!');
 });
 
 app.get('/topping/:type', function(req, res, next) {
-  res.send(req.params.type + " pizza! Good choice");
+  // res.send(req.params.type + " pizza! Good choice");
+  res.render();
 });
 
 app.get('/order/:amount/:size', function(req, res, next) {
-  res.send('Your order for '+req.params.amount+' '+req.params.size+' pizzas will be ready in 1 minute!');
+  // res.send('Your order for '+req.params.amount+' '+req.params.size+' pizzas will be ready in 1 minute!');
+  res.render();
 });
+
+app.get('')
 
 app.listen(port, function(){
   console.log("==========================")
