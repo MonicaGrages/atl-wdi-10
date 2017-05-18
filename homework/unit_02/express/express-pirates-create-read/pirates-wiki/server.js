af0a9ccc11ca38c55e26ffeea84a24a7fde1a3a9
MonicaGrages <mgrages1@gmail.com>
2017-05-18 17:44:33 -0400

    added some css. it looks really bad.

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -2,6 +2,7 @@
 //your code here
 var express = require('express');
 var hbs = require('hbs');
+var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
@@ -9,19 +10,17 @@ var port = 3000;
 var methodOverride = require('method-override');
 var pirateController = require('./controllers/pirates.js');
 
-
 app.use(bodyParser.urlencoded({
   extended: false
 }));
-  app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use( logger('dev'));
 app.set('view engine', 'hbs');
 app.set('views', './views');
-
+app.use(express.static(__dirname + '/public'));
 
 app.use(methodOverride('_method'));
 app.use('/pirates', pirateController);
-
 
 
 app.get('/', function(req,res) {
