a19c5d02dbabf4b519b84a71abcafad4c17867fe
MonicaGrages <mgrages1@gmail.com>
2017-05-18 07:53:40 -0400

    got index and show views working. new pirate view working but not showing on index after redirect

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,1 +1,51 @@
 //your code below
+var express = require('express');
+var router = express.Router();
+var seededPirates = require('../models/pirates.js'); //what?
+
+
+
+// all routes for /pirates go here
+
+
+router.get('/', function (request, response) {
+  //index route
+  // this should show all the pirates
+  response.render('pirates/index', {
+    seededPirates : seededPirates
+  });
+});
+
+
+router.get('/new', function (request, response) {
+  //a form should appear here
+  response.render('pirates/new');
+});
+
+router.get('/:id', function(request, response) { //this is the show route and has to be below the new route
+  //show one pirate
+  var theChosenPirate = seededPirates[request.params.id];
+  response.render('pirates/show', {
+      name : theChosenPirate.name,
+      birthplace : theChosenPirate.birthplace,
+      death_year : theChosenPirate.death_year,
+      base : theChosenPirate.base,
+      nickname : theChosenPirate.nickname
+  });
+});
+
+router.post('/', function (request, response) {
+  //things happen here
+  seededPirates.push(request.body)
+  response.redirect('/pirates');
+});
+
+
+
+module.exports = router;
+
+
+
+
+
+
--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,1 +1,35 @@
 //your code here
+var express = require('express');
+var hbs = require('hbs');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var app = express();
+var port = 3000;
+var pirateController = require('./controllers/pirates.js');
+
+app.use( logger('dev'));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+app.use('/pirates', pirateController);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, function(){
+  console.log("==========================")
+  console.log('LISTENING ON PORT ' + port);
+  console.log("==========================")
+});
