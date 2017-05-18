a313035df07795e7fdd248f997c5c688b184f450
MonicaGrages <mgrages1@gmail.com>
2017-05-18 15:38:52 -0400

    added edit route and PUT for editing pirates

--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/controllers/pirates.js
@@ -1,9 +1,7 @@
 //your code below
 var express = require('express');
 var router = express.Router();
-var seededPirates = require('../models/pirates.js'); //what?
-
-
+var seededPirates = require('../models/pirates.js');
 
 // all routes for /pirates go here
 
@@ -34,21 +32,46 @@ router.get('/:id', function(request, response) { //this is the show route and ha
   });
 });
 
+
+//get pirate making form
 router.post('/', function (request, response) {
   //things happen here
   var addedPirate = request.body;
-
   seededPirates.push(addedPirate);
-
   response.redirect('/pirates');
 });
 
+//get pirate editing form
+router.get('/:id/edit', function (request, response) {
+  response.render('pirates/edit', {
+    pirate : {
+      name : seededPirates[request.params.id].name,
+      birthplace : seededPirates[request.params.id].birthplace,
+      death_year : seededPirates[request.params.id].death_year,
+      base : seededPirates[request.params.id].base,
+      nickname : seededPirates[request.params.id].nickname,
+      id : request.params.id
+    }
+  });
+});
 
+//edit the pirate after filling out edit form
+router.put('/:id', function(request, response) {
+  var pirateToEdit = seededPirates[request.params.id];
+  console.log('pirate to edit is: '+pirateToEdit.name);
+  pirateToEdit.name = request.body.name;
+  pirateToEdit.birthplace = request.body.birthplace;
+  pirateToEdit.death_year = request.body.death_year;
+  pirateToEdit.base = request.body.base;
+  pirateToEdit.nickname = request.body.nickname;
+  console.log('now the edited pirate is: '+pirateToEdit.name);
+  response.redirect('/pirates/'+request.params.id);
+});
+
+router.delete('/:id', function (request, response) {
+  seededPirates.splice(request.params.id, 1);
+  response.redirect('/pirates');
+});
 
 module.exports = router;
-
-
-
-
-
 
--- homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
+++ homework/unit_02/express/express-pirates-create-read/pirates-wiki/server.js
@@ -1,3 +1,4 @@
+
 //your code here
 var express = require('express');
 var hbs = require('hbs');
@@ -5,7 +6,9 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
 var port = 3000;
+var methodOverride = require('method-override');
 var pirateController = require('./controllers/pirates.js');
+
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -14,19 +17,16 @@ app.use(bodyParser.urlencoded({
 app.use( logger('dev'));
 app.set('view engine', 'hbs');
 app.set('views', './views');
+
+
+app.use(methodOverride('_method'));
 app.use('/pirates', pirateController);
 
 
 
-
-
-
-
-
-
-
-
-
+app.get('/', function(req,res) {
+  res.send('This is our Home Page');
+});
 
 
 app.listen(port, function(){
