184d7734667ecc577357053fd7b12ea9f01c526f
MonicaGrages <mgrages1@gmail.com>
2017-05-22 13:23:53 -0400

    part 2: added arguments to Tamagotchi and part 3: added new methods

--- homework/unit_02/javaScript/js-constructor/tamagotchi.js
+++ homework/unit_02/javaScript/js-constructor/tamagotchi.js
@@ -1,20 +1,30 @@
 console.log('tamagotchi file is loaded');
 
 //your constructor function here
-function Tamagotchi() {
+function Tamagotchi(name, creatureType) {
   this.foodInTummy = 10;
   this.restedness = 10;
   this.health = 10;
   this.cry = function() {
+    console.log(this.name+' says WAHH!!!');
+  };
+  this.name = name;
+  this.creatureType = creatureType;
+  this.puke = function () {
     this.foodInTummy --;
-    console.log('food in tummy: '+this.foodInTummy);
-    console.log('WAHH!!!');
-  }
-}
+    console.log("food in "+this.name+"'s tummy: "+this.foodInTummy);
+    this.cry;
+  };
+  this.yawn = function () {
+    this.restedness --;
+    console.log(this.name+"'s restedness is: "+this.restedness);
+  };
+};
 
 //create new Tamagotchis
-var tamagotchi1 = new Tamagotchi();
-var tamagotchi2 = new Tamagotchi();
+var fred = new Tamagotchi('Fred', 'monster');
+var fire = new Tamagotchi('Fire', 'dragon');
+var beep = new Tamagotchi('Beep', 'robot');
 
 //test out your Tamagotchies below via console.logs
-tamagotchi1.cry();
+fred.cry();
