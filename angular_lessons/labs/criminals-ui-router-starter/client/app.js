9de94840969c8f759367dd089a13ea68a19fae0f
MonicaGrages <mgrages1@gmail.com>
2017-06-07 11:23:33 -0400

    added some comments to starter code

--- angular_lessons/labs/criminals-ui-router-starter/client/app.js
+++ angular_lessons/labs/criminals-ui-router-starter/client/app.js
@@ -7,11 +7,12 @@ angular
 
 uiRouterSetup.$inject = ['$stateProvider', '$urlRouterProvider'];
 
+//this is where we set up the different states of our app
 function uiRouterSetup($stateProvider, $urlRouterProvider) {
 	$stateProvider
 		.state('home', {
-			url: '/',
-			template: '<home></home>'
+			url: '/', //this is the url you want in your browser
+			template: '<home></home>' //this is the name of the component you created
 		})
 		.state('about', {
 			url: '/about',
