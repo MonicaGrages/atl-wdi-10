c71d5749f7bf22e5225a315a3304a6b5f709c4dc
MonicaGrages <mgrages1@gmail.com>
2017-06-07 14:42:25 -0400

    just fixed merge conflict

--- homework/unit_03/angular/ron-swanson/angular_ui_router/app.js
+++ homework/unit_03/angular/ron-swanson/angular_ui_router/app.js
@@ -3,6 +3,7 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
+var path = require('path');
 var port = 3000;
 
 mongoose.connect('mongodb://localhost:27017/angular-hw-app');
@@ -17,24 +18,6 @@ app.use('/quotes', quotesController);
 
 var netflixController = require('./server/routes/netflix.js');
 app.use('/netflix', netflixController);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 app.listen(port, function(){
   console.log('listening on port ' + port);
