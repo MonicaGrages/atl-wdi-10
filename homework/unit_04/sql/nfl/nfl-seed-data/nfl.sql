419b30bed34dba80f50a2a619ac7c5219e1b79a9
MonicaGrages <mgrages1@gmail.com>
2017-06-14 17:43:34 -0400

    queried some NFL stuff

---
+++ homework/unit_04/sql/nfl/nfl-seed-data/nfl.sql
@@ -0,0 +1,4 @@
+1. nfl=# SELECT name FROM teams;
+2. nfl=# SELECT stadium, head_coach FROM teams;
+3. nfl=# SELECT stadium, head_coach FROM teams WHERE conference  = 'NFC';
+4. nfl=# SELECT COUNT(id) FROM players;
