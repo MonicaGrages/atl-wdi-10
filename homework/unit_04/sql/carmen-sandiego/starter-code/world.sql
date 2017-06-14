1e63441beec6bae9935cdcf8d89fe06524ef7ecd
MonicaGrages <mgrages1@gmail.com>
2017-06-14 17:33:15 -0400

    I seeded the database with the players and teams info

--- homework/unit_04/sql/carmen-sandiego/starter-code/world.sql
+++ homework/unit_04/sql/carmen-sandiego/starter-code/world.sql
@@ -44,7 +44,7 @@ CREATE TABLE countrylanguage (
 );
 
 COPY city (id, name, countrycode, district, population) FROM stdin;
-1   Kabul   AFG Kabol   1780000
+1   Kabul   AFG Kabol  1780000
 2   Qandahar    AFG Qandahar    237500
 3   Herat   AFG Herat   186800
 4   Mazar-e-Sharif  AFG Balkh   127800
