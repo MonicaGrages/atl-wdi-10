5dcd9588f2cb3479eb208ad5879efe36277b5014
MonicaGrages <mgrages1@gmail.com>
2017-06-14 22:43:32 -0400

    Realty schema created.

---
+++ homework/unit_04/sql/realty/realty_schema.sql
@@ -0,0 +1,39 @@
+DROP TABLE IF EXISTS apartments;
+DROP TABLE IF EXISTS offices;
+DROP TABLE IF EXISTS storefronts;
+
+CREATE TABLE apartments (
+  id SERIAL PRIMARY KEY,
+  apt_number INTEGER,
+  bedrooms INTEGER,
+  bathrooms INTEGER,
+  address VARCHAR NOT NULL,
+  tenant VARCHAR,
+  occupied BOOLEAN,
+  sq_footage INTEGER,
+  price INTEGER
+);
+
+CREATE TABLE office (
+  id SERIAL PRIMARY KEY,
+  office_number INTEGER,
+  floors INTEGER,
+  sq_footage INTEGER,
+  cubicles INTEGER,
+  bathrooms INTEGER,
+  address VARCHAR NOT NULL,
+  company_name VARCHAR,
+  occupied BOOLEAN,
+  price INTEGER
+);
+
+CREATE TABLE storefront (
+  id SERIAL PRIMARY KEY,
+  address VARCHAR NOT NULL,
+  occupied BOOLEAN,
+  price INTEGER,
+  has_kitchen BOOLEAN,
+  sq_footage INTEGER,
+  owner VARCHAR,
+  has_outdoor_seating BOOLEAN DEFAULT FALSE
+);
