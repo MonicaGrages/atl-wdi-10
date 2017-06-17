63a46102b8d57484c89e485f935b0afb4d161f12
MonicaGrages <mgrages1@gmail.com>
2017-06-17 19:08:38 -0400

    Nil skills

--- homework/unit_04/ruby/ninja_skills.rb
+++ homework/unit_04/ruby/ninja_skills.rb
@@ -1,4 +1,11 @@
-a = true
-b = false
-# puts a == b
-puts a != b
+# a = true
+# b = false
+# # puts a == b
+# puts a != b
+
+
+some_variable = nil
+puts some_variable
+puts "#{some_variable} nil was here"
+print some_variable
+puts some_variable.class
