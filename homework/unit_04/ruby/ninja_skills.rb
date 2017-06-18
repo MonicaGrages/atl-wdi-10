fe1b6e2809ebf440eb20755ded9b4735dd035af0
MonicaGrages <mgrages1@gmail.com>
2017-06-17 20:51:02 -0400

    Integers skills

--- homework/unit_04/ruby/ninja_skills.rb
+++ homework/unit_04/ruby/ninja_skills.rb
@@ -4,8 +4,21 @@
 # puts a != b
 
 
-some_variable = nil
-puts some_variable
-puts "#{some_variable} nil was here"
-print some_variable
-puts some_variable.class
+# some_variable = nil
+# puts some_variable
+# puts "#{some_variable} nil was here"
+# print some_variable
+# puts some_variable.class
+
+# Fixnum is deprecated
+# d = Fixnum
+# puts d
+# e = Integer
+# puts e
+# f = 5
+# puts 4.class
+# g = 5.0
+# puts g.class
+# f = g
+# puts f
+
