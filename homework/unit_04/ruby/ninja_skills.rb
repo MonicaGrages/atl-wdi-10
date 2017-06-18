1d7607cb620e64c8f4fa6b71f55eaabf9662d04d
MonicaGrages <mgrages1@gmail.com>
2017-06-18 15:26:13 -0400

    String skills

--- homework/unit_04/ruby/ninja_skills.rb
+++ homework/unit_04/ruby/ninja_skills.rb
@@ -1,15 +1,17 @@
+#CHALLENGE 1
 # a = true
 # b = false
 # # puts a == b
 # puts a != b
 
-
+#CHALLENGE 2
 # some_variable = nil
 # puts some_variable
 # puts "#{some_variable} nil was here"
 # print some_variable
 # puts some_variable.class
 
+#CHALLENGE 3
 # Fixnum is deprecated
 # d = Fixnum
 # puts d
@@ -22,3 +24,14 @@
 # f = g
 # puts f
 
+#CHALLENGE 4
+# christine = "Hi, I'm Christine"
+# likes_to = "long walks on the beach, kinda"
+# puts "#{christine} and I like #{likes_to}"
+# g = "2"
+# g_to_number = 2
+# puts g
+# puts g_to_number
+# puts g_to_number + g
+# puts g + g_to_number
+# puts g_to_number + g.to_i
